cf020df8aa1d7876b408a3650670433d7e09091e
Nikhil03-hub <anthatinikhil03@gmail.com>
2026-06-25 02:33:51 +0530

    Week 4 UI changes

--- week-4-intelligent-code-reviewer/app.py
+++ week-4-intelligent-code-reviewer/app.py
@@ -31,6 +31,7 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(mess
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", os.urandom(32))
 app.config["MAX_CONTENT_LENGTH"] = 200 * 1024  # 200 KB upload limit
 
